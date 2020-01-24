5a5d5a99b47d0acb0bc27805c34ac6749b16f5d9
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-24 13:28:39 +0100

    Add project files.

---
+++ CalcoliCombinatori/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcoliCombinatori")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcoliCombinatori")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("753eb510-0ad8-4656-99e6-7e906f5428f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EquazioniLibrary/CalcoliCombinatori.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class CalcoliCombinatori
+    {
+        public static long Fattoriale (int n)
+        {
+           if (n == 0)
+            {
+                return 1;
+            }
+           else if(n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int Fattoriale = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    Fattoriale *= i;
+                }
+                return Fattoriale;
+            }
+        }
+    }
+}
